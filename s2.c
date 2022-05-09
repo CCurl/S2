fe8b8769c857154a36edddc0feafd487f62a2d30
Chris Curl <curlbud-github@yahoo.com>
2022-05-09 08:35:21 -0400

    Update s2.c

--- s2.c
+++ s2.c
@@ -1,94 +1,133 @@
-// S2.c - inspired by, and based on, STABLE from Sandor Schneider
-#define _CRT_SECURE_NO_WARNINGS    // For Visual Studio
-#include <windows.h>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
-#define btw(a,b,c) ((b<=a) && (a<=c))
-#define TOS st.i[s]
-#define NOS st.i[s-1]
-#define SZ 10000
-union flin { float f[SZ/4]; int i[SZ/4]; char b[SZ]; }; static union flin st;
-static char ex[80], *y; static int c, h, r, cb=SZ-3000, p, s, ro=64, rb=35, sb=3, t, u;
-/* <33 */ void X()   { if (u && (u!=10)) printf("-IR %d (%c)?", u, u); p=0; } void N() {}
-/*  !  */ void f33() { st.i[TOS]=NOS; s-=2; }
-/*  "  */ void f34() { while (st.b[p]!='"') { putc(st.b[p++], stdout); } ++p; }
-/*  #  */ void f35() { t=TOS; st.i[++s]=t; }
-/*  $  */ void f36() { t=TOS; TOS=NOS; NOS=t; }
-/*  %  */ void f37() { t=NOS; st.i[++s]=t; }
-/*  &  */ void f38() { u=NOS; t=TOS; NOS=u/t; TOS=u%t; }
-/*  '  */ void f39() { st.i[++s]=st.b[p++]; }
-/*  (  */ void f40() { if (st.i[s--]==0) { while (st.b[p++]!=')'); } }
-/*  *  */ void f42() { NOS *= TOS; s--; }
-/*  +  */ void f43() { NOS += TOS; s--; }
-/*  ,  */ void f44() { putc(st.i[s--], stdout); }
-/*  -  */ void f45() { NOS -= TOS; s--; }
-/*  .  */ void f46() { printf("%d", st.i[s--]); }
-/*  /  */ void f47() { NOS /= TOS; s--; }
-/* 0-9 */ void n09() { st.i[++s]=(u-'0'); while (btw(st.b[p],'0','9')) { TOS=(TOS*10)+st.b[p++]-'0'; }
-            if (st.b[p]=='e') { ++p; st.f[s]=(float)TOS; } }
-/*  :  */ void f58() { u=st.b[p++]; if (btw(u,'A','Z')) { st.i[u]=p; while (st.b[p++]!=';'); if (h<p) h=p; } }
-/*  ;  */ void f59() { p=st.i[r--]; if (r<rb) { r=rb; p=0; } }
-/*  <  */ void f60() { t=TOS; u=NOS; s--; TOS=(u<t)?-1:0; if (st.b[p]=='=') { ++p; TOS=(u<=t)?-1:0; } }
-/*  =  */ void f61() { NOS=(NOS==TOS)?-1:0; s--; }
-/*  <  */ void f62() { t=TOS; u=NOS; s--; TOS=(u>t)?-1:0; if (st.b[p]=='=') { ++p; TOS=(u>=t)?-1:0; } }
-/*  ?  */ void f63() { c=fgetc(stdin); st.i[++s]=(c!=EOF)?c:0; }
-/*  @  */ void f64() { TOS=st.i[TOS]; }
-/* A-Z */ void AZ()  { if (st.i[u]) { if (st.b[p]!=';') { st.i[++r]=p; } p=st.i[u]; } }
-/*  [  */ void f91() { st.i[++r]=p; st.i[++r]=st.i[s--]; st.i[++r]=st.i[s--]; }
-/*  \  */ void f92() { --s; }
-/*  ]  */ void f93() { ++st.i[r]; if (st.i[r] <= st.i[r-1]) { p=st.i[r-2]; } else { r-=3; } }
-/*  ^  */ void f94() { p=st.i[r--]; }
-/*  _  */ void f95() { TOS=-TOS; }
-/*  `  */ void f96() { y=ex; while ((31<st.b[p]) && (st.b[p]!='`')) { *(y++)=st.b[p++]; } *y=0; ++p; system(ex); }
-/*  b  */ void f98() { u=st.b[p++]; if (u=='~') { TOS = ~TOS; }
-            else if (u=='&') { NOS&=TOS; s--; }
-            else if (u=='|') { NOS|=TOS; s--; }
-            else if (u=='^') { NOS^=TOS; s--; }
-            else { putc(32, stdout); --p; } }
-/*  c  */ void f99()  { u=st.b[p++]; if (u=='@') { TOS=st.b[TOS]; } else if (u=='!') { st.b[TOS]=NOS; s -= 2; } }
-/*  d  */ void f100() { t=ro+st.b[p++]; st.i[t]--; if (st.b[p]=='@') { st.i[++s]=st.i[t]; ++p; } }
-/*  e  */ void f101() { st.i[++r]=p; p=st.i[s--]; }
-/*  f  */ void f102() { u=st.b[p++];                             if (u=='.') { printf("%f", st.f[s--]); }
-            else if (u=='@') { st.f[s]=st.f[TOS]; }        else  if (u=='!') { st.f[TOS]=st.f[s-1]; s-=2; }
-            else if (u=='+') { st.f[s-1]+=st.f[s]; s--; }  else  if (u=='<') { TOS=(st.f[s-1] < st.f[s]) ? -1 : 0; }
-            else if (u=='-') { st.f[s-1]-=st.f[s]; s--; }  else  if (u=='>') { TOS=(st.f[s-1] > st.f[s]) ? -1 : 0; }
-            else if (u=='*') { st.f[s-1]*=st.f[s]; s--; }  else  if (u=='i') { TOS=(int)st.f[s]; }
-            else if (u=='/') { st.f[s-1]/=st.f[s]; s--; }  else  if (u=='f') { st.f[s]=(float)TOS; }
-            else if (u=='O') { y=&st.b[NOS]; t=TOS; NOS=(int)fopen(y, (t)?"wb":"rb"); s--; }
-            else if (u=='C') { if (TOS) { fclose((FILE*)TOS); } s--; }
-            else if (u=='R') { s++; TOS=0; if (NOS) fread((void*)&TOS, 1, 1, (FILE*)NOS); }
-            else if (u=='W') { if (TOS) { fwrite((void*)&NOS, 1, 1, (FILE*)TOS); } s-=2; } }
-/*  i  */ void f105() { t=ro+st.b[p++]; st.i[t]++; if (st.b[p]=='@') { st.i[++s]=st.i[t]; ++p; } }
-/*  l  */ void f108() { u=st.b[p++]; if (u=='@') { TOS=*((int*)TOS); }  /*else if (u=='!') { *((int *)TOS)=NOS; s-=2; }*/ }
-/*  m  */ void f109() { u=st.b[p++]; if (u=='@') { TOS=*((char*)TOS); } /*else if (u=='!') { *((char*)TOS)=NOS; s-=2; }*/ }
-/*  q  */ void f113() { for (int i=sb; i<=s; i++) { printf("%c%d", (i==sb)?0:32, st.i[i]); } }
-/*  r  */ void f114() { t=ro+st.b[p++]; st.i[++s]=st.i[t]; }
-/*  s  */ void f115() { t=ro+st.b[p++]; st.i[t]=st.i[s--]; }
-/*  t  */ void f116() { st.i[++s]=GetTickCount(); }
-/*  x  */ void f120() { u=st.b[p++]; if (u=='I') { st.i[++s]=st.i[r]; }
-            else if (u=='U') { --r; }
-            else if (u=='W') { while (st.b[p++]!='}'); r--; }
-            else if (u=='F') { while (st.b[p++]!=']'); r-=3; }
-            else if (u=='%') { NOS %= TOS; s--; }
-            else if (u=='Q') { exit(0); } }
-/*  {  */ void f123() { st.i[++r]=p; if (TOS==0) { while (st.b[p]!='}') { ++p; } } }
-/*  }  */ void f125() { if (TOS) { p=st.i[r]; } else { --r; --s; } }
-/*  ~  */ void f126() { TOS = (TOS) ? 0 : -1; }
-void (*q[127])()={ X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,N,f33,f34,f35,f36,f37,f38,
-    f39,f40,N,f42,f43,f44,f45,f46,f47,n09,n09,n09,n09,n09,n09,n09,n09,n09,n09,f58,f59,f60,f61,f62,f63,f64,
-    AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,AZ,f91,f92,f93,f94,f95,f96,X,
-    f98,f99,f100,f101,f102,X,X,f105,X,X,f108,f109,X,X,X,f113,f114,f115,f116,X,X,X,f120,X,X,f123,X,f125,f126 };
-void R(int x) { s=(s<sb)?(sb-1):s; r=rb; p=x; while (p) { u=st.b[p++]; q[u](); } }
-void H(char *s) { FILE *fp=fopen("h.txt", "at"); if (fp) { fprintf(fp, "%s", s); fclose(fp); } }
-void L() { y=&st.b[h]; printf("\ns2:("); f113(); printf(")>"); fgets(y, 128, stdin); H(y); R(h); }
-void main(int argc, char *argv[]) {
-    s=sb-1; h=cb; u=800; for (int i=0; i<(SZ/4); i++) { st.i[i]=0; }
-    for (int i = 1; i < argc; ++i) { y=argv[i]; t=atoi(y); 
-        if ((t) || (y[0]=='0' && y[1]==0)) { st.i[ro+'0'+i]=t; }
-        else { st.i[ro+'0'+i]=u; for (int j=0; y[j]; j++) { st.b[u++]=y[j]; } st.b[u++]=0; } }
-    if ((argc>1) && (argv[1][0]!='-')) { FILE *fp=fopen(argv[1], "rb"); 
-        if (fp) {while ((c=fgetc(fp))!=EOF) { if (btw(c,32,126)) st.b[h++]=c; } fclose(fp); R(cb); } else { printf("file?"); }
-    } while (1) { L(); };
-}
+# S2
+S2 is a full-featured and interactive stack-based interpreter/VM, implemented in less than 100 lines of C code.
+
+It was inspired by, and is based on, Sandor Schneider's STABLE program.
+
+S2 supports up to 26 function definitions (A..Z), floating point math, and simple file operations. It also provides 95 registers.
+
+## Examples
+```
+0(this is a comment)
+"Hello World!"     0("Hello World!")
+10_ 10[xI.b]       0(print out numbers from -10 through 10)
+#("yes")~("no")    0(print "yes" or "no" depending on TOS)
+r1 fO#(fR{,fR}fC)  0(print the contents of the file named by r1)
+123 {#.b 1-}       0(count down and print out from 123 to 0)
+355e 113e f/ F.    0(floating point - PI)
+:N10,;             0(define function N)
+32 126[xI#.":".,N] 0(print the ascii table)
+```
+## S2 Reference
+```
+*** STACK ***
+#  (a--a a)       Duplicate TOS             (DUP)
+\  (a b--a)       Drop TOS                  (DROP)
+$  (a b--b a)     Swap top 2 stack items    (SWAP)
+%  (a b--a b a)   Push 2nd                  (OVER)
+_  (a--b)         b: -a                     (NEGATE)
+
+
+*** ARITHMETIC ***
++   (a b--n)      n: a+b
+-   (a b--n)      n: a-b
+*   (a b--n)      n: a*b
+/   (a b--q)      q: a/b
+&   (a b--q r)    q:: DIV(a,b), r:MOD(a,b)  (/MOD)
+x%  (a b--r)      r: MOD(a,b)
+
+
+*** FLOATING POINT ***
+0-9e (--n)        n: a floating point number
+ff   (a--b)       b: int a converted to float
+fi   (a--b)       b: float a converted to int
+f+   (a b--n)     n: a+b
+f-   (a b--n)     n: a-b
+f*   (a b--n)     n: a*b
+f/   (a b--q)     q: a/b
+f<   (a b--a f)   f: (a < b) ? -1 : 0;
+f>   (a b--a f)   f: (a > b) ? -1 : 0;
+
+
+*** BIT MANIPULATION ***
+b&  (a b--n)      n: a AND b
+b|  (a b--n)      n: a OR  b
+b^  (a b--n)      n: a XOR b
+b~  (a--b)        b: NOT a (ones-complement, e.g - 11001011 => 00110100)
+
+
+*** MEMORY ***
+@     (a--n)      Fetch INT   n from S2 address a
+c@    (a--n)      Fetch BYTE  n from S2 address a
+f@    (a--n)      Fetch FLOAT n from S2 address a
+m@    (a--n)      Fetch BYTE  n from ABSOLUTE address a
+l@    (a--n)      Fetch INT   n from ABSOLUTE address a
+!     (n a--)     Store INT   n to S2 address a
+c!    (n a--)     Store BYTE  n to S2 address a
+f!    (n a--)     Store FLOAT n to S2 address a
+m!    (n a--)     Store BYTE  n to ABSOLUTE address a
+l!    (n a--)     Store INT   n to ABSOLUTE address a
+        NOTE: m!, and l! may cause the virus scanner to freak out.
+              If so, you can comment out the (u=='!') part of functions f108() and f109().
+
+
+*** REGISTERS ***
+        NOTES: 1) A register name is any printable character, including <space>
+               2) Punctuation characters can also be used registers
+rX    (--n)       Read value of register X (n)
+sX    (n--)       Store (n) in register X
+iX    (--)        Increment register X
+iX@   (--n)       n: value of register X after incrementing it
+dX    (--)        Decrement register X
+dX@   (--n)       n: value of register X after decrementing it
+
+
+*** WORDS/FUNCTIONS ***
+        NOTES: 1) A function name is a single UPPERCASE character
+:     (--)        Define function. Copy chars to (HERE++) until closing ';'.
+X     (?--?)      Call function X
+;     (--)        Function definition end, return
+^     (--)        Early return from function
+        NOTES: 1) When in a FOR loop, unwind the loop stack first using (xUxUxU^)
+               2) When in a WHILE loop, unwind the loop stack first using (xU^)
+
+
+*** INPUT/OUTPUT ***
+.      (n--)      n: Number to output as a decimal
+,      (c--)      c: Character to output
+b      (--)       Output a single SPACE (NOTE: b&, b|, b^, and b~ take precedence)
+".."   (--)       Output characters until the next '"'.
+0..9   (--n)      Scan DECIMAL number n. For multiple numbers, separate them by space (47 33).
+        NOTES: 1) To enter a negative number, use "NEGATE" (eg - 490_).
+               2) If "e" immediately follows the number (eg - 355e), then n is converted to a float.
+'x     (--n)      n: the ASCII value of x
+`XXX`  (--)       Executes XXX as a shell command (ie - system(xxx))
+?      (--c)      c: next character from STDIN (0 if EOF)
+
+
+*** CONDITIONS/LOOPS/FLOW CONTROL ***
+<     (a b--f)    f: (a <  b) ? -1 : 0;
+=     (a b--f)    f: (a == b) ? -1 : 0;
+>     (a b--f)    f: (a >  b) ? -1 : 0;
+<=    (a b--f)    f: (a <= b) ? -1 : 0;
+>=    (a b--f)    f: (a >= b) ? -1 : 0;
+~     (a--f)      f: (a == 0) ? -1 : 0;  (Logical NOT)
+(     (f--)       IF: if (f != 0), continue into '()', else jump to next ')'
+[     (F T--)     FOR: start a FOR/NEXT loop.
+]     (--)        NEXT: increment index (I) and restart loop if (rI <= T)
+xI    (--n)       n: the index of the current FOR loop iterator
+xF    (--)        eXit FOR loop: unwind FOR loop stack, jump to next ']'
+{     (f--f)      BEGIN: if (f == 0) jump to matching '}'
+}     (f--f?)     WHILE: if (f != 0) jump to matching '{', else drop f and continue
+xW    (--)        eXit WHILE loop: unwind WHILE loop stack, continue after next '}'
+e     (A--)       EXECUTE: call function at location A
+
+*** FILE ***
+fO    (a n--f)    OPEN  - n: 0=>READ, else WRITE (usage: 1000 0fO)
+fC    (f--)       CLOSE - f: file handle
+fR    (f--f c)    FREAD - f: file handle, c: char read (0 if EOF)
+fW    (c f--)     WRITE - f: file handle, c: char to write
+
+
+*** OTHER ***
+xQ    (--)        Exit S2
+```
