7ee25d6ad3f670cf8374fc36a0fc329ed6719041
Chris Curl <curlc@yahoo.com>
2022-11-22 23:50:46 -0500

    j-bench updates

--- j-bench.c
+++ j-bench.c
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
   long pp= argc>1 ? atol(argv[1]) : 2124680;
 
-  // 710 ms (android clang -O3)
+  // 832 ms (android clang -O3)
   for(long a=2; a<=100; a++) {
     long b=pp-1;
     for(long c=2; c<=b; c++) {
